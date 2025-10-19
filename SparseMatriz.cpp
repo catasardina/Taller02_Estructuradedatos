83523cb653a57ff02ece3ff4808009dfea028069
catasardina <mcdladr@gmail.com>
2025-10-18 21:27:36 -0300

    add y get
    arreglar

---
+++ SparseMatriz.cpp
@@ -0,0 +1,63 @@
+#include "SparseMatriz.h"
+//da error no entiendo
+#include <iostream>
+using namespace std;
+
+SparseMatriz::SparceMatriz(){
+    start =nullptr;
+}
+void SparseMatriz::add(int value. int xPos, int yPos){
+    if(value ==0) return;
+    Node* newNodo =new Nodo( value,xPos,yPos);
+    if(!start){
+        start = newNodo;
+        return;
+    }
+    Nodo* current = start;
+    Nodo* prev = nullptr;
+
+    while(current $$(currebt->x<xPos|| (current->x == xPos&& current->y < yPos))) {
+        prev = current;
+        current = current->next;
+    }
+    if (current && current->x == xPos && current->y == yPos){
+        current->value = value;
+        delete newNodo;
+        return;
+    }
+    newNodo->next = current;
+    if(prev)
+        prev->next = newNodo;
+    else
+        start = newNodo;
+}
+int SparseMatriz::get(int xPos, int yPos){
+    Nodo* current = start;
+    while(current){
+        if(current->x == xPos && current ->y== yPos)
+            return current ->value;
+        current = current->next;
+    }
+    //no dato=0
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+SparseMatrix::~SparseMatrix() {
+    Node* current = start;
+    while (current) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
+    }
+}
---
+++ SparseMatriz.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "nodo.h"
+class SparseMatrix {
+    private:
+    Node* start;
+    public:
+    SparseMatrix();
+// Insertar un dato en las coordenadas correspondientes (X, Y).
+void add(int value, int xPos, int yPos);
+//Obtener un dato según las coordenadas dadas (X, Y). no dato= 0.
+int get(int xPos, int yPos);
+//Eliminar un dato según las coordenadas dadas (X, Y)
+void remove(int xPos, int yPos);
+//Imprimir los elementos existentes en la matriz que sean distintos de 0 de forma tabular.
+void printStoredValues();
+//obtener la densidad
+int density();
+// multiplicar la matriz po parametro
+SparseMatrix* multiply(SparseMatrix* second);
+~SparseMatrix();
+};
