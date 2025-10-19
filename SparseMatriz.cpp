d4f040dc850f17fc789662ed2ba5a535babffa07
catasardina <mcdladr@gmail.com>
2025-10-19 19:57:45 -0300

    Metodos matrix
    falta probarlos

--- SparseMatriz.cpp
+++ SparseMatriz.cpp
@@ -41,6 +41,68 @@ int SparseMatrix::get(int xPos, int yPos){
     //no dato=0
     return 0;
 }
+void SparseMatrix::remove(int xPos, int yPos) {
+    Node* current = start;
+    Node* prev = nullptr;
+
+    while (current) {
+        if (current->x == xPos && current->y == yPos) {
+            if (prev) prev->next = current->next;
+            else start = current->next;
+            delete current;
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+void SparseMatrix::printStoredValues() {
+    Node* current = start;
+    if (!current) {
+        cout << "La matriz está vacía." << endl;
+        return;
+    }
+    while (current) {
+        cout << "(" << current->x << ", " << current->y << ") --> " << current->value << endl;
+        current = current->next;
+    }
+}
+float SparseMatrix::density() {
+    if (!start) return 0.0f;
+
+    int count = 0;
+    int maxX = 0, maxY = 0;
+
+    Node* current = start;
+    while (current) {
+        count++;
+        if (current->x > maxX) maxX = current->x;
+        if (current->y > maxY) maxY = current->y;
+        current = current->next;
+    }
+
+    int totalSize = (maxX + 1) * (maxY + 1);
+    return (float)count / totalSize;
+}
+
+SparseMatrix SparseMatrix::multiply(SparseMatrix* second){
+    SparseMatrix* result = new SparseMatrix();
+    Nodo* a = start;
+    while(a){
+        Nodo* b = second->start;
+        while(b){
+            if(a->y == b->x){
+                int currentVal = result->get(a->x, b->y);
+                result->add ( currentVal + (a->value *b->value), a->x,b->y);
+            }
+            b= b->next;
+
+        }
+        a=a->next;
+    }
+
+    return result;
+}
 
 
 
